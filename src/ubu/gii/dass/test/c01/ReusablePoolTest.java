63c412a72d6968f5e5d45574e5a771242b79a6bb
Anton Epikhin <aex0022@alu.ubu.es>
2016-02-27 17:07:34 +0100

    Añadida documentación y formateo del código en TestReusablePool.java

--- src/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -11,8 +11,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * @author alumno
- *
+ * Clase Test que prueba la clase ReusablePool con 100% de cobertura.
+ * 
+ * @author CUEVAS DÍEZ JOSÉ RAMÓN
+ * @author LÓPEZ MARÍN LAURA
+ * @author CUADRADO GARCÍA IRENE
+ * @author EPIKHIN ANTON
  */
 public class ReusablePoolTest {
 
@@ -51,13 +55,14 @@ public class ReusablePoolTest {
 	public void testGetInstance() {
 		// Adquiere instancia de ReusablePool.
 		ReusablePool poolTest = ReusablePool.getInstance();
-		
+
 		// Comprueba que pool y poolTest son instancia de ReusablePool.
 		assertTrue(pool instanceof ReusablePool);
 		assertTrue(poolTest instanceof ReusablePool);
-		
+
 		// Compara los dos objetos son de la misma instancia.
-		// Debido a que el metodo 'equals' no es sobreescrito, 'equals' y '==' darán el mismo resultado.
+		// Debido a que el metodo 'equals' no es sobreescrito, 'equals' y '=='
+		// darán el mismo resultado.
 		assertTrue(pool == poolTest);
 		assertTrue(pool.equals(poolTest));
 	}
@@ -66,42 +71,44 @@ public class ReusablePoolTest {
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
 	 */
 	@Test
-	public void testAcquireReusable() {	
-			try {				
-				//acquirir correctamente
-				pool.releaseReusable(reusable2);				
-				Reusable reusable3=pool.acquireReusable();
-				
-				//acquirir sin posibilidad de hacerlo, NotFreeInstance
-				Reusable reusable4=pool.acquireReusable();
-				
-			} catch (NotFreeInstanceException e) {
-				assertEquals("No hay más instancias reutilizables disponibles. Reintentalo más tarde",e.getMessage());
-			} catch (DuplicatedInstanceException e) {
-				assertEquals("Ya existe esa instancia en el pool.",e.getMessage());
-			}
-	
+	public void testAcquireReusable() {
+		try {
+			// acquirir correctamente
+			pool.releaseReusable(reusable2);
+			Reusable reusable3 = pool.acquireReusable();
+
+			// acquirir sin posibilidad de hacerlo, NotFreeInstance
+			Reusable reusable4 = pool.acquireReusable();
+
+		} catch (NotFreeInstanceException e) {
+			assertEquals("No hay más instancias reutilizables disponibles. Reintentalo más tarde", e.getMessage());
+		} catch (DuplicatedInstanceException e) {
+			assertEquals("Ya existe esa instancia en el pool.", e.getMessage());
+		}
+
 	}
 
 	/**
 	 * Test method for
 	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}
 	 * .
-	 * @throws NotFreeInstanceException 
+	 * 
+	 * @throws NotFreeInstanceException
 	 */
 	@Test
 	public void testReleaseReusable() throws NotFreeInstanceException {
-				try{
-					//se añade al pool un objeto reusable, correcto
-					pool.releaseReusable(reusable1);
-					//se intenta añadir el mismo objeto reusable, DuplicatedInstanceException
-					pool.releaseReusable(reusable1);
-				}catch (DuplicatedInstanceException e){
-					assertEquals("Ya existe esa instancia en el pool.",e.getMessage());
-					}
-				
-				// se saca el objeto reusable para dejar el pool vacio (estado inicial)
-				reusable1=pool.acquireReusable();
+		try {
+			// se libera del pool un objeto reusable, correcto
+			pool.releaseReusable(reusable1);
+			// se intenta liberar el mismo objeto reusable,
+			// DuplicatedInstanceException
+			pool.releaseReusable(reusable1);
+		} catch (DuplicatedInstanceException e) {
+			assertEquals("Ya existe esa instancia en el pool.", e.getMessage());
+		}
+
+		// se añade el objeto reusable para dejar el pool vacio (estado inicial)
+		reusable1 = pool.acquireReusable();
 	}
 
 }
