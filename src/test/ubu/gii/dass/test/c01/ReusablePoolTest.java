ce9169ba8b204094568d1f48240a89a60166d786
Carlos López <clopezno@ubu.es>
2018-02-01 20:29:55 +0100

    Gestión de tareas con ant para integrar con Travis CI

---
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package ubu.gii.dass.test.c01;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author alumno
+ *
+ */
+public class ReusablePoolTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
+	 */
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
+	 */
+	@Test
+	public void testAcquireReusable() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 */
+	@Test
+	public void testReleaseReusable() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/ubu/gii/dass/c01/DuplicatedInstanceException.java
+++
@@ -1,9 +0,0 @@
-package ubu.gii.dass.c01;
-
-public class DuplicatedInstanceException extends Exception {
-	
-	public  DuplicatedInstanceException(){
-		super("Ya existe esa instancia en el pool.");
-	}
-
-}
