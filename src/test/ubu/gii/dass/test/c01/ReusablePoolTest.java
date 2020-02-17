90bd9472dc87d12e566460be9b95ead706fbed78
danielsetorey <daniel.seto@gmail.com>
2020-02-17 18:08:04 +0100

    Implementación método SetUp de inicialización de tests

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -5,9 +5,16 @@ package ubu.gii.dass.test.c01;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import ubu.gii.dass.c01.DuplicatedInstanceException;
+import ubu.gii.dass.c01.NotFreeInstanceException;
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
 
 /**
  * @author alumno
@@ -16,10 +23,18 @@ import org.junit.Test;
 public class ReusablePoolTest {
 
 	/**
+	 * Inicialización de test
 	 * @throws java.lang.Exception
 	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception {		
+		// Utilizando Reflection podemos asegurarnos de trabajar siempre
+		// con una "nueva" instancia de Singleton en cada Test.
+		// Esta solución no me gusta demasiado, pero no veo cómo
+		// pueden hacerse las pruebas realmente independientes si no es así.
+		Field instance = ReusablePool.class.getDeclaredField("instance");
+		instance.setAccessible(true);
+		instance.set(null, null);
 	}
 
 	/**
