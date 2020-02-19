806e45995821b7c241b06b3fa8fe08bfc2017e48
dsr0018 <dsr0018@alu.ubu.es>
2020-02-19 12:17:14 +0100

    Test GetInstance
    Comprueba el funcionamiento correcto del patrón Singleton

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,6 +9,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+
+import ubu.gii.dass.c01.ReusablePool;
+
 /**
  * @author alumno
  *
@@ -16,10 +20,20 @@ import org.junit.Test;
 public class ReusablePoolTest {
 
 	/**
+	 * Inicialización de test
 	 * @throws java.lang.Exception
 	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception {		
+		// Utilizando Reflection podemos asegurarnos de trabajar siempre
+		// con una "nueva" instancia de Singleton en cada Test.
+		// Esta solución no me gusta demasiado, pero no veo cómo
+		// pueden hacerse las pruebas realmente independientes si no es así.
+		// (Porque no es posible garantizar que una prueba conserve
+		// el estado interno de una instancia Singleton)
+		Field instance = ReusablePool.class.getDeclaredField("instance");
+		instance.setAccessible(true);
+		instance.set(null, null);
 	}
 
 	/**
@@ -31,10 +45,18 @@ public class ReusablePoolTest {
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
+	 * Comprueba el funcionamiento correcto del patrón Singleton
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
+		ReusablePool pool = ReusablePool.getInstance();
+		assertNotNull("Se ha devuelto un objeto nulo", pool);
+		
+		ReusablePool pool2 = ReusablePool.getInstance();
+		assertNotNull("Se ha devuelto un objeto nulo", pool2);
+		
+		assertSame("Se espera la misma instancia para el Singleton", pool, pool2);
+		
 	}
 
 	/**
