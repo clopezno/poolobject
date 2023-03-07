ca67e1a3095c6e34a2ab88abcf6ffeb4d5b9a542
jmr1006-ubu <93679537+jmr1006-ubu@users.noreply.github.com>
2023-03-07 18:40:03 +0100

    Update Client.java

--- src/main/ubu/gii/dass/c01/Client.java
+++ src/main/ubu/gii/dass/c01/Client.java
@@ -8,7 +8,7 @@ public class Client{
 
 	public static void main(String arg[]) throws NotFreeInstanceException, DuplicatedInstanceException{
 		ReusablePool pool;
-		Reusable r1,r2,r3;
+		Reusable r1,r2,r3 = null;
 		Logger logger = Logger.getLogger("c01");
 
 
@@ -19,7 +19,21 @@ public class Client{
 
 		logger.info(r1.util());
 		logger.info(r2.util());
+		logger.info(r2.util());
+		logger.info(r2.util());
 
+
+		if (r1 != r2) {
+		   if (r2 != r3) {
+		      if (r1 != r3) {
+			     if (2 < 3) {
+			    	 logger.info(r2.util());
+			    	 logger.info(r2.util());
+			    	 logger.info(r2.util());
+			     }
+		      }
+		   }
+		}
 
 
 		pool.releaseReusable(r2);
