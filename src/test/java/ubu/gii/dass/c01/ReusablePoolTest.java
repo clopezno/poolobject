3880f17a09356ac9d6bbdaacacdd32e08df0cd51
CARLOS LOPEZ NOZAL <clopezno@ubu.es>
2024-04-26 11:50:22 +0200

    #11 migrate to junit 4 to 5 jupyter and adjust package of classe

---
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package ubu.gii.dass.c01;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Disabled;
+
+
+
+/**
+ * @author alumno
+ *
+ */
+public class ReusablePoolTest {
+
+	
+	@BeforeAll
+	public static void setUp(){
+	}
+
+	
+	@AfterAll
+	public static void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
+	 */
+        @Test
+        @DisplayName("testGetInstance")
+        @Disabled("Not implemented yet")
+	public void testGetInstance() {
+		
+	}
+
+	/**
+	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
+	 */
+	@Test
+        @DisplayName("testAcquireReusable")
+        @Disabled("Not implemented yet")
+
+	public void testAcquireReusable() {
+		
+	}
+
+	/**
+	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 */
+	@Test
+        @DisplayName("testReleaseReusable")
+        @Disabled("Not implemented yet")
+	public void testReleaseReusable() {
+		
+	}
+
+}
