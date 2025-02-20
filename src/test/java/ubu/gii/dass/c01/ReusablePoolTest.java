5473a87014f75af445a6e8e361aa65b285f0c4d8
César Rodriguez Villagrá <crv1002@alu.ubu.es>
2025-02-20 18:19:32 +0100

    @DisplayName("testAcquireReusable") Añadido el test de adquirir reusable y formateado el archivo

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -5,6 +5,8 @@ package ubu.gii.dass.c01;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,20 +14,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Disabled;
 
-
-
 /**
  * @author alumno
  *
  */
 public class ReusablePoolTest {
 
-	
 	@BeforeAll
-	public static void setUp(){
+	public static void setUp() {
 	}
 
-	
 	@AfterAll
 	public static void tearDown() throws Exception {
 	}
@@ -33,35 +31,45 @@ public class ReusablePoolTest {
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
 	 */
-        @Test
-        @DisplayName("testGetInstance")
+	@Test
+	@DisplayName("testGetInstance")
 	public void testGetInstance() {
-        ReusablePool pool1 = ReusablePool.getInstance();
-        ReusablePool pool2 = ReusablePool.getInstance();
-        assertNotNull(pool1);
-        assertNotNull(pool2);
-        assertEquals(pool1, pool2);
+		ReusablePool pool1 = ReusablePool.getInstance();
+		ReusablePool pool2 = ReusablePool.getInstance();
+		assertNotNull(pool1);
+		assertNotNull(pool2);
+		assertEquals(pool1, pool2);
 	}
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
 	 */
 	@Test
-        @DisplayName("testAcquireReusable")
-        @Disabled("Not implemented yet")
+	@DisplayName("testAcquireReusable")
 
 	public void testAcquireReusable() {
-		
+		ReusablePool pool = ReusablePool.getInstance();
+		try {
+			for (int i = 0; i < 2; i++) {
+				pool.acquireReusable();
+			}
+		} catch (NotFreeInstanceException e) {
+			fail("No deberia de llegar aqui");
+		}
+		assertThrows(NotFreeInstanceException.class, () -> {
+			pool.acquireReusable();
+		});
 	}
 
 	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * Test method for
+	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
 	 */
 	@Test
-        @DisplayName("testReleaseReusable")
-        @Disabled("Not implemented yet")
+	@DisplayName("testReleaseReusable")
+	@Disabled("Not implemented yet")
 	public void testReleaseReusable() {
-		
+
 	}
 
 }
