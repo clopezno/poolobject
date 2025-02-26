b23e4e964bb305c82f2d00b346479b1b0502d6e1
cvo0004 <cvo0004@alu.ubu.es>
2025-02-26 19:52:41 +0100

    testGetInstance

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -3,6 +3,9 @@
  */
 package ubu.gii.dass.c01;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
@@ -14,7 +17,7 @@ import org.junit.jupiter.api.Disabled;
 
 
 /**
- * @author alumno
+ * @author  <A HREF="mailto:cvo0004@alu.ubu.es">Carlos Venero Ortega</A>
  *
  */
 public class ReusablePoolTest {
@@ -34,9 +37,21 @@ public class ReusablePoolTest {
 	 */
         @Test
         @DisplayName("testGetInstance")
-        @Disabled("Not implemented yet")
 	public void testGetInstance() {
+        //Declaro dos objetos ReusablePool y obtengo las instancias de esa clase para cada objeto.
+        ReusablePool miPool = ReusablePool.getInstance();;
+        ReusablePool miPool2 = ReusablePool.getInstance();;
         
+        //Compruebo que ambas instancias no son null.
+        assertNotNull(miPool, "miPool no es una null.");
+        assertNotNull(miPool2, "miPool2 no es una null.");
+        
+        //Compruebo que ambas instancias son realmente instancias de ReusablePool.
+        assertTrue(miPool instanceof ReusablePool, "miPool es una instancia de ReusablePool.");
+        assertTrue(miPool2 instanceof ReusablePool, "miPool2 es una instancia de ReusablePool.");
+        
+        //Compruebo que ambas instancias son realmente la misma y apuntan al mismo objeto en memoria.
+        assertSame(miPool, miPool2, "Ambas instancias apuntan al mismo objeto en memoria.");
 	}
 
 	/**
