59907a6e732dfea5fa0f5d688a1f59f05ffde00a
JuanUBU <jgl1012@alu.ubu.es>
2025-02-27 17:39:23 +0100

    Tests reusable y release
    Los tests de release y reusables

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -1,70 +1,55 @@
-/**
- * 
- */
 package ubu.gii.dass.c01;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 
-
-
-/**
- * @author alumno
- *
- */
 public class ReusablePoolTest {
 
-	@BeforeAll
-	public static void setUp(){
-	}
+    @BeforeAll
+    public static void setUp(){
+    }
 
-	
-	@AfterAll
-	public static void tearDown() throws Exception {
-	}
+    @AfterAll
+    public static void tearDown() throws Exception {
+    }
 
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
-	 * Comprobamos que se cree la instancia en pool1 y que pool2 recibe el mismo objeto que pool1.
-	 */
-        @Test
-        @DisplayName("testGetInstance")
-	public void testGetInstance() {
-		ReusablePool pool1 = ReusablePool.getInstance();
-		ReusablePool pool2 = ReusablePool.getInstance();
-		assertNotNull(pool1);
-		assertNotNull(pool2);
-		assertEquals(pool1, pool2, "Both pools are identical");
-	}
+    @Test
+    @DisplayName("testGetInstance")
+    public void testGetInstance() {
+        ReusablePool pool1 = ReusablePool.getInstance();
+        ReusablePool pool2 = ReusablePool.getInstance();
+        assertNotNull(pool1);
+        assertNotNull(pool2);
+        assertEquals(pool1, pool2, "Los dos son identicos");
+    }
 
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
-	 */
-	@Test
-        @DisplayName("testAcquireReusable")
-        @Disabled("Not implemented yet")
+    @Test
+    @DisplayName("testAcquireReusable")
+    public void testAcquireReusable() {
+        ReusablePool pool = ReusablePool.getInstance();
+        Reusable obj1 = pool.acquireReusable();
+        Reusable obj2 = pool.acquireReusable();
 
-	public void testAcquireReusable() {
-		
-	}
+        assertNotNull(obj1, "El primero no deberia ser null");
+        assertNotNull(obj2, "El segundo no deberia ser null");
+        assertNotSame(obj1, obj2, "Los objetos deberian ser diferentes");
+    }
 
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
-	 */
-	@Test
-        @DisplayName("testReleaseReusable")
-        @Disabled("Not implemented yet")
-	public void testReleaseReusable() {
-		
-	}
-
+    @Test
+    @DisplayName("testReleaseReusable")
+    public void testReleaseReusable() {
+        ReusablePool pool = ReusablePool.getInstance();
+        Reusable obj1 = pool.acquireReusable();
+        
+        assertNotNull(obj1, "Objeto no deberia ser null");
+        pool.releaseReusable(obj1);
+        
+        Reusable obj2 = pool.acquireReusable();
+        assertSame(obj1, obj2, "No deberia ser null");
+    }
 }
